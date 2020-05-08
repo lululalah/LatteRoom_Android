443c54dc315f0499bf554ac96f3ab5159ebfd057
lululalah <hyojinlee1007@gmail.com>
2020-05-08 10:04:21 +0900

    UI 0507

--- app/src/main/java/com/example/androidlatteroom/MainActivity.java
+++ app/src/main/java/com/example/androidlatteroom/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.androidlatteroom;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button main_light = (Button)findViewById(R.id.main_light);
+        main_light.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                ComponentName cname = new ComponentName
+                        ("com.example.androidlatteroom",
+                                "com.example.androidlatteroom.lightActivity");
+                i.setComponent(cname);
+                startActivity(i);
+            }
+        });
+
+        Button main_bed = (Button)findViewById(R.id.main_bed);
+        main_light.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                ComponentName cname = new ComponentName
+                        ("com.example.androidlatteroom",
+                                "com.example.androidlatteroom.bedActivity");
+                i.setComponent(cname);
+                startActivity(i);
+            }
+        });
+
+        Button main_alarm = (Button)findViewById(R.id.main_alarm);
+        main_light.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                ComponentName cname = new ComponentName
+                        ("com.example.androidlatteroom",
+                                "com.example.androidlatteroom.alarmActivity");
+                i.setComponent(cname);
+                startActivity(i);
+            }
+        });
+
+        Button main_thermo = (Button)findViewById(R.id.main_thermo);
+        main_light.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                ComponentName cname = new ComponentName
+                        ("com.example.androidlatteroom",
+                                "com.example.androidlatteroom.thermoActivity");
+                i.setComponent(cname);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode ==3000 && resultCode==7000){
+            String msg = (String)data.getExtras().get("ResultValue");
+
+            Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+        }
     }
 }
