9825fe44d354dcd51781ac1ec5bcec1f7ab53455
Hae-gun <newchk610@gmail.com>
2020-05-08 11:16:19 +0900

    my

--- app/src/main/java/com/example/androidlatteroom/MainActivity.java
+++ app/src/main/java/com/example/androidlatteroom/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.androidlatteroom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView light = findViewById(R.id.mainLight);
+        light.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                ComponentName cname = new ComponentName("com.example.androidlatteroom",
+                        "com.example.androidlatteroom.LightActivity");
+                i.setComponent(cname);
+                startActivity(i);
+            }
+        });
+
     }
 }
