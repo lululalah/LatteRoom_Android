cb479f31b6de203a03361abd781400308d2539b9
lululalah <hyojinlee1007@gmail.com>
2020-05-09 10:25:26 +0900

    0508 UI

--- app/src/main/java/com/example/androidlatteroom/climateActivity.java
+++ app/src/main/java/com/example/androidlatteroom/climateActivity.java
@@ -3,15 +3,39 @@ package com.example.androidlatteroom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class climateActivity extends AppCompatActivity {
 
+    private Socket socket;
+    private BufferedReader br;
+    private PrintWriter pr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_climate);
 
+
+    Runnable r = ()->{
+        try {
+            socket = new Socket("70.12.60.111",55566);
+            br = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+            pr = new PrintWriter(socket.getOutputStream());
+            Log.i("ServerTest","서버에 접속성공");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 
